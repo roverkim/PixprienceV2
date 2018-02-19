6c9b749a5b6d6db23299605289da0d394ca85a3c
KiwonNam2016 <kiwonnam2016@u.northwestern.edu>
2018-02-19 07:43:38 -0600

    map

--- client/src/components/TimelineMap/Map.js
+++ client/src/components/TimelineMap/Map.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import PropTypes from "prop-types";
-
+import Marker from "./Marker"
 
 class Map extends React.Component{
 
@@ -24,6 +24,7 @@ class Map extends React.Component{
         }
     }
     
+    // function that initializes the map
     loadMap(){
         if(this.props && this.props.google){
             const {google} = this.props;
