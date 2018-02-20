e0b52d37f0a493e98c4e9a2d0a327e6bce93ebb8
roverkim <ryankim@hotmail.com>
2018-02-20 00:07:37 -0600

    Revert "map with markers but minimal functionality, date added to carousel"

--- client/src/components/TimelineMap/MapContainer.js
+++ client/src/components/TimelineMap/MapContainer.js
@@ -4,52 +4,6 @@ import Map from "./Map";
 
 class MapContainer extends React.Component{
 
-    // constructor
-    constructor() {
-        super();
-        this.state = {
-            
-            //this is an object that will hold null until all the data is properly received
-            imageData:[]
-        }
-        this.fetchTimelineImages = this.fetchTimelineImages.bind(this);
-    }
-    componentWillMount() {
-        this.fetchTimelineImages();
-    }
-
-    // fetch the images and all the data from the backend
-    fetchTimelineImages() { // Function to Fetch Timeline Images
-
-
-        let clientEmail = localStorage.getItem('userEmail');
-        axios.post('/test/images', {params: { email: clientEmail }})
-        // axios.get('/test/images')
-            .then( response => {
-            // console.log(response)
-            this.setState({
-                imageData: response.data
-            });
-              
-    
-            //   this.setState({
-            //     asyncImages: this.state.timeline_images.map(base64_image => base64_image.image) // Replace AysncImages Null with an Array of Images taken from
-            //   })
-    
-            //   // console.log(this.state.timeline_images)
-
-            // take the images that come from the back end and then convert the location to lat/lng and then store the new object in new array
-            
-    
-            })
-            .catch(function (error) {
-              console.log(error);
-        });
-    }
-
-
-
-
     render(){
         // if(!this.props.loaded){
         //     return <div>Loading</div>
